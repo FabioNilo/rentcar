e7602d1b8717447f4342f09b3a52276f03acb3e0
FabioNilo <fabionilosoares@gmail.com>
2025-07-10 12:46:34 -0300

    atualizando imagens

--- src/data.js
+++ src/data.js
@@ -5,7 +5,14 @@ import FrangoES from "./assets/IMFE2.jpg";
 import FrangoDes from "./assets/IMFD.jpg";
 import FrangoA from "./assets/IMGFA.jpg";
 import FrangoPQ from "./assets/IMGPF.jpg";
-
+import BifeMolho from './assets/BifeMolho.jpg'
+import CarneSol from './assets/CarneSol.jpg'
+import Lombo from './assets/LomboRecheado.jpg'
+import CoxaFrango from './assets/CoxaFrango.jpg'
+import IscaCarne from './assets/IscaCarne.jpg'
+import FileTilapia from './assets/TilapiaFile.jpg'
+import FileTilapia2 from './assets/TilapiaFile2.jpg'
+import FrangoXadrez from './assets/FrangoXadrez.jpg'
 
 export  const QUENTINHAS = [
     {
@@ -68,5 +75,69 @@ export  const QUENTINHAS = [
       price: 20, 
       image: FrangoPQ
     },
+    {
+    id: 8,
+    name: "Quentinha Bife ao Molho",
+    protein: "Bife ao molho",
+    ingredients: "Bife ao molho, batata sauté, arroz e feijão",
+    price: 20,
+    image: BifeMolho
+  },
+  {
+    id: 9,
+    name: "Quentinha Lombo Recheado",
+    protein: "Lombo recheado",
+    ingredients: "Lombo recheado, seleta de legumes refogada, arroz e feijão",
+    price: 20,
+    image: Lombo
+  },
+  {
+    id: 10,
+    name: "Quentinha Coxas Assadas com Maionese",
+    protein: "Coxas de frango assadas com maionese",
+    ingredients: "Coxas de frango assadas com maionese, purê de batatas, mix de legumes e abóbora cabotiá assada",
+    price: 20,
+    image: CoxaFrango
+  },
+  {
+    id: 11,
+    name: "Quentinha Isca ao Molho Madeira",
+    protein: "Isca de carne ao molho madeira",
+    ingredients: "Isca de carne ao molho madeira, purê de aimpim e seleta de legumes refogada",
+    price: 20,
+    image: IscaCarne
+  },
+  {
+    id: 12,
+    name: "Quentinha Carne do Sol",
+    protein: "Carne do sol assada",
+    ingredients: "Carne do sol assada, purê de banana da terra, arroz e mix de legumes",
+    price: 20,
+    image: CarneSol
+  },
+  {
+    id: 13,
+    name: "Quentinha Tilápia com Ervas",
+    protein: "Filé de tilápia com molho de ervas",
+    ingredients: "Filé de tilápia com molho de ervas, purê de banana da terra, arroz e mix de legumes",
+    price: 20,
+    image: FileTilapia
+  },
+  {
+    id: 14,
+    name: "Quentinha Tilápia Cremosa",
+    protein: "Filé de tilápia com legumes e molho cremoso",
+    ingredients: "Filé de tilápia com legumes e molho cremoso, purê de abóbora e arroz",
+    price: 20,
+    image: FileTilapia2
+  },
+  {
+    id: 15,
+    name: "Quentinha Frango Xadrez",
+    protein: "Frango xadrez",
+    ingredients: "Frango xadrez, purê de batatas e mix de legumes",
+    price: 20,
+    image: FrangoXadrez
+  }
   
   ];
